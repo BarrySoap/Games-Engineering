c924b68be5d703796843e6aa851960f3392c4ad1
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-01-09 16:21:52 +0000

    Added a load function for a game of pong.

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,20 +1,31 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works.");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-		window.clear();
-		window.draw(shape);
-		window.display();
+using namespace sf;
+using namespace std;
+
+const Keyboard::Key controls[4] = {
+	Keyboard::W,	// Player1 UP
+	Keyboard::S,	// Player1 DOWN
+	Keyboard::Up,	// Player2 UP
+	Keyboard::Down	// Player2 DOWN
+};
+
+const Vector2f paddleSize(25.0f, 100.0f);
+const float ballRadius = 10.0f;
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float paddleSpeed = 400.0f;
+
+CircleShape ball;
+RectangleShape paddles[2];
+
+void Load() {
+	// Set size and origin of paddles
+	for (auto &paddle : paddles) {
+		paddle.setSize(paddleSize - Vector2f(3, 3));
+		paddle.setOrigin(paddleSize / 2.0f);
 	}
-	return 0;
+	// Set size and origin of ball
+	ball.setRadius(ballRadius - 3);
+	ball.setOrigin(ballRadius - Vector2f(3, 3));
 }
