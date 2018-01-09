51f244cdaa38b890783f6c6a1dcb4c9dac1c4041
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-01-09 16:27:00 +0000

    Added an update function for pong.

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -11,13 +11,13 @@ const Keyboard::Key controls[4] = {
 };
 
 const Vector2f paddleSize(25.0f, 100.0f);
-const float ballRadius = 10.0f;
-const int gameWidth = 800;
-const int gameHeight = 600;
-const float paddleSpeed = 400.0f;
+const float ballRadius = 10.0f;				// Size of ball / 2
+const int gameWidth = 800;					// Screen Width
+const int gameHeight = 600;					// Screen Height
+const float paddleSpeed = 400.0f;			// Paddle movement speed
 
-CircleShape ball;
-RectangleShape paddles[2];
+CircleShape ball;							// Ball object
+RectangleShape paddles[2];					// Container for paddles
 
 void Load() {
 	// Set size and origin of paddles
@@ -27,5 +27,39 @@ void Load() {
 	}
 	// Set size and origin of ball
 	ball.setRadius(ballRadius - 3);
-	ball.setOrigin(ballRadius - Vector2f(3, 3));
+	ball.setOrigin(ballRadius / 2, ballRadius / 2);
+	// Reset paddle position
+	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+	//paddles[1].setPosition(...);
+	// Reset ball position
+	//ball.setPosition(...);
+}
+
+void Update(RenderWindow &window) {
+	// Reset clock, recalculate deltaTime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	// Check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+
+	// Quit via ESC key
+	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+		window.close();
+	}
+
+	// Handle paddle movement
+	float direction = 0.0f;
+	if (Keyboard::isKeyPressed(controls[0])) {
+		direction--;
+	}
+	if (Keyboard::isKeyPressed(controls[1])) {
+		direction++;
+	}
+	paddles[0].move(0, direction * paddleSpeed * dt);
 }
