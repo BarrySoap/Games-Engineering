ca0c0f504a80a25adced0298a9d9c19a0b1458dd
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-03-27 23:33:03 +0100

    Sprite component source/header files written.

---
+++ practical_4/cmp_sprite.cpp
@@ -0,0 +1,19 @@
+#include "cmp_sprite.h"
+#include "system_renderer.h"
+
+using namespace std;
+using namespace sf;
+
+void SpriteComponent::update(double dt) {
+	_sprite->setPosition(_parent->getPosition());
+}
+
+void ShapeComponent::update(double dt) {
+	_shape->setPosition(_parent->getPosition());
+}
+
+void ShapeComponent::render() { Renderer::queue(_shape.get()); }
+
+sf::Shape& ShapeComponent::getShape() const { return *_shape; }
+
+ShapeComponent::ShapeComponent(Entity* p) : Component(p), _shape(make_shared < sf::CircleShape()) {}
